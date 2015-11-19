49996d561ba8e2fdd1e25981c81651b30efe53f4
Stefan <stefan.thorpe@gmail.com>
2015-11-19 17:41:14 +0700

    correct typos and add additional logging for debug

--- nfs/recipes/nfs_server_setup.rb
+++ nfs/recipes/nfs_server_setup.rb
@@ -24,19 +24,23 @@
     creates "#{node[:opsworks][:nfs][:export_full_path]}"
   end
      
+ grep_results_size = File.readlines("/etc/fstab").grep(/caylent-nfs-exports/).size
+ grep_results = File.readlines("/etc/fstab").grep(/caylent-nfs-exports/)
+ Chef::Log.info "Wizkru-Setup:THe size of the grep query: #{grep_results_size}. It's actual results #{grep_results}"
+     
   if (File.exists?("/etc/exports") && File.readlines("/etc/exports").grep(/caylent-nfs-exports/).size < 1) #ToDo improve check and update
     
     Chef::Log.info "Caylent-Setup: No entry found in the /etc/exports adding them now"
     execute 'add export root to exports file' do
-      command 'echo "#caylent-nfs-exports" > /etc/exports'
+      command 'echo "#caylent-nfs-exports" >> /etc/exports'
     end
    
     execute 'add export root to exports file' do
-      command "echo '#{node[:opsworks][:nfs][:export_root]} 172.31.0.0/16(rw,fsid=0,no_subtree_check,sync)' > /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
+      command "echo '#{node[:opsworks][:nfs][:export_root]} 172.31.0.0/16(rw,fsid=0,no_subtree_check,sync)' >> /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
     end
     
     execute 'add export full path to exports file' do
-      command "echo '#{node[:opsworks][:nfs][:export_root]} 172.31.0.0/16(rw,nohide,insecure,no_subtree_check,sync)' > /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
+      command "echo '#{node[:opsworks][:nfs][:export_root]} 172.31.0.0/16(rw,nohide,insecure,no_subtree_check,sync)' >> /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
     end
     
     execute 'restart nfs kernal' do
