3555f25fbba29398fdd43303133f41acf9b64422
Stefan <stefan.thorpe@gmail.com>
2015-11-20 10:31:06 +0700

    added method and called once deployed

--- nfs/recipes/nfs_server_setup.rb
+++ nfs/recipes/nfs_server_setup.rb
@@ -18,42 +18,44 @@
     when 'ubuntu', 'debian' #ToDo extend to other platforms
       package_name 'nfs-kernel-server'
     end
+    notifies :run, nfs_mount, :immediatly
   end
  
- 
- Chef::Log.info "Caylent-Setup: Create export root folder #{node[:opsworks][:nfs][:export_root]}"
-  execute "create export root folder" do 
-    command "mkdir #{node[:opsworks][:nfs][:export_root]}"
-    creates "#{node[:opsworks][:nfs][:export_root]}"
-  end
-  
-  Chef::Log.info "Caylent-Setup: Create export folder #{node[:opsworks][:nfs][:export_full_path]}"
-  execute "create export folder" do 
-    command "mkdir #{node[:opsworks][:nfs][:export_full_path]}"
-    creates "#{node[:opsworks][:nfs][:export_full_path]}"
-  end
+ def nfs_mount do
+   Chef::Log.info "Caylent-Setup: Create export root folder #{node[:opsworks][:nfs][:export_root]}"
+    execute "create export root folder" do 
+      command "mkdir #{node[:opsworks][:nfs][:export_root]}"
+      creates "#{node[:opsworks][:nfs][:export_root]}"
+    end
+    
+    Chef::Log.info "Caylent-Setup: Create export folder #{node[:opsworks][:nfs][:export_full_path]}"
+    execute "create export folder" do 
+      command "mkdir #{node[:opsworks][:nfs][:export_full_path]}"
+      creates "#{node[:opsworks][:nfs][:export_full_path]}"
+    end
+       
+   grep_results_size = File.readlines("/etc/fstab").grep(/caylent-nfs-exports/).size
+   grep_results = File.readlines("/etc/fstab").grep(/caylent-nfs-exports/)
+   Chef::Log.info "Wizkru-Setup:THe size of the grep query: #{grep_results_size}. It's actual results #{grep_results}"
+       
+    if (File.exists?("/etc/exports") && File.readlines("/etc/exports").grep(/caylent-nfs-exports/).size < 1) #ToDo improve check and update
+      
+      Chef::Log.info "Caylent-Setup: No entry found in the /etc/exports adding them now"
+      execute 'add export root to exports file' do
+        command 'echo "#caylent-nfs-exports" >> /etc/exports'
+      end
      
- grep_results_size = File.readlines("/etc/fstab").grep(/caylent-nfs-exports/).size
- grep_results = File.readlines("/etc/fstab").grep(/caylent-nfs-exports/)
- Chef::Log.info "Wizkru-Setup:THe size of the grep query: #{grep_results_size}. It's actual results #{grep_results}"
-     
-  if (File.exists?("/etc/exports") && File.readlines("/etc/exports").grep(/caylent-nfs-exports/).size < 1) #ToDo improve check and update
-    
-    Chef::Log.info "Caylent-Setup: No entry found in the /etc/exports adding them now"
-    execute 'add export root to exports file' do
-      command 'echo "#caylent-nfs-exports" >> /etc/exports'
-    end
-   
-    execute 'add export root to exports file' do
-      command "echo '#{node[:opsworks][:nfs][:export_root]} 172.31.0.0/16(rw,fsid=0,no_subtree_check,sync)' >> /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
-    end
-    
-    execute 'add export full path to exports file' do
-      command "echo '#{node[:opsworks][:nfs][:export_full_path]} 172.31.0.0/16(rw,nohide,insecure,no_subtree_check,sync)' >> /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
-    end
-    
-    execute 'restart nfs kernal' do
-      command 'service nfs-kernel-server restart && service idmapd restart'
+      execute 'add export root to exports file' do
+        command "echo '#{node[:opsworks][:nfs][:export_root]} 172.31.0.0/16(rw,fsid=0,no_subtree_check,sync)' >> /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
+      end
+      
+      execute 'add export full path to exports file' do
+        command "echo '#{node[:opsworks][:nfs][:export_full_path]} 172.31.0.0/16(rw,nohide,insecure,no_subtree_check,sync)' >> /etc/exports" #ToDo Replace fixed subnet masks with dynamic from stack
+      end
+      
+      execute 'restart nfs kernal' do
+        command 'service nfs-kernel-server restart && service idmapd restart'
+      end
     end
   end
 
