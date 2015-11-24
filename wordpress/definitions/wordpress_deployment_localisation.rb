5248cb8c3f47acdae9ef77eec0ac36b43fa7994b
Stefan <stefan.thorpe@gmail.com>
2015-11-24 08:56:44 +0700

    uncomment link removal and added permissino change

--- wordpress/definitions/wordpress_deployment_localisation.rb
+++ wordpress/definitions/wordpress_deployment_localisation.rb
@@ -37,10 +37,10 @@ define :wordpress_deployment_localisation do
 
   def remove_current_symlink
     Chef::Log.info "Caylent-Deploy: Do Nothing"
-    #execute "remove and replace currentsymlink" do
-    #  command "rm #{node[:deploy][$application][:current_path]} && mkdir #{node[:deploy][$application][:current_path]}"
-    #  
-    #end
+    execute "remove and replace currentsymlink" do
+      command "rm #{node[:deploy][$application][:current_path]} && mkdir #{node[:deploy][$application][:current_path]} && chmod -R 777 #{node[:deploy][$application][:current_path]}"           
+    end
+    
   end
  
  
