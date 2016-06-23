e5dc1522f19a73753dfd6bdd9380aa3c3bc2bb64
Stefan Thorpe <stefan.thorpe@gmail.com>
2016-06-23 15:22:22 -0400

    trying run not action

--- docker/definitions/docker_deployment_localisation.rb
+++ docker/definitions/docker_deployment_localisation.rb
@@ -42,7 +42,7 @@ define :docker_deployment_localisation do
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
         command = 'aws ecr get-login'
         node.default[:deploy][application][:docker_login] = shell_out(command)
-        notifies :action, 'execute[docker-login]', :immediately
+        notifies :run, 'execute[docker-login]', :immediately
     end
     action :run
   end
