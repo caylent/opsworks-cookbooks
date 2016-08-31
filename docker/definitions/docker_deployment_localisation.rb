ffab936e519265baa4cd68123f0f0a4dd50d6534
Julia Pearson <julia.pearson@gmail.com>
2016-08-31 18:53:20 -0400

    missed cycle in production

--- docker/definitions/docker_deployment_localisation.rb
+++ docker/definitions/docker_deployment_localisation.rb
@@ -99,7 +99,10 @@ define :docker_deployment_localisation do
  if (node[:deploy][application][:environment_variables][:ENV] == "prod")
   Chef::Log.info "Caylent-Deploy: Dirty fix for first boot"
   execute "dirty-start" do
-    command "docker run #{ports} #{env_commands} --name #{docker_containerName} #{docker_url}/#{docker_application}:#{docker_version} #{config_commands}"
+    deploy_commands.each.with_index(1) do |deply_command, index|
+      command "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} #{deploy_command}"
+    end
+    command "docker run #{ports} #{env_commands} --name #{docker_containerName} #{docker_url}/#{docker_application}:#{docker_version}"
     ignore_failure true
   end
   Chef::Log.info "Caylent-Deploy: Dirty fix for first boot"
@@ -122,8 +125,8 @@ define :docker_deployment_localisation do
   # else
     Chef::Log.info "Caylent-Deploy: Attempting to run image"
     execute "run image" do
-      deploy_commands.each.with_index(1) do |command, index|
-        command "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} #{command}"
+      deploy_commands.each.with_index(1) do |deply_command, index|
+        command "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} #{deploy_command}"
       end
       #command "docker run -p 80:80 -p 443:443 #{node[:deploy][application][:environment_variables][:docker_image]}:#{node[:deploy][application][:environment_variables][:docker_version]}"
       command "docker run -d #{env_commands} #{ports} --name #{docker_containerName} #{docker_url}/#{docker_application}:#{docker_version}"
