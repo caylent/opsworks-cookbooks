73433fcbae199c297fe1b7fb0ac7285fdff70a63
Julia Pearson <julia.pearson@gmail.com>
2016-09-01 14:03:19 -0400

    in own execute loop

--- docker/definitions/docker_deployment_localisation.rb
+++ docker/definitions/docker_deployment_localisation.rb
@@ -97,13 +97,17 @@ define :docker_deployment_localisation do
   end
 
  if (node[:deploy][application][:environment_variables][:ENV] == "prod")
+  deploy_commands.each.with_index(1) do |deploy_command, index|
+    execute "deploy commands" do
+      docker_with_command = "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} '#{deploy_command}'"
+      Chef::Log.info "Running #{deploy_command} with #{index}"
+      Chef::Log.info docker_with_command
+      
+      command docker_with_command
+    end
+  end
   Chef::Log.info "Caylent-Deploy: Dirty fix for first boot"
   execute "dirty-start" do
-    deploy_commands.each.with_index(1) do |deploy_command, index|
-      Chef::Log.info "Running #{deploy_command} with #{index}"
-      Chef::Log.info "DOCKER COMMAND: docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} '#{deploy_command}'"
-      command "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} #{deploy_command}"
-    end
     command "docker run #{ports} #{env_commands} --name #{docker_containerName} #{docker_url}/#{docker_application}:#{docker_version}"
     ignore_failure true
   end
@@ -118,7 +122,15 @@ define :docker_deployment_localisation do
     command "docker rm #{docker_containerName}-old; docker stop #{docker_containerName} && docker rename #{docker_containerName} #{docker_containerName}-old"
     ignore_failure true
   end
+  deploy_commands.each.with_index(1) do |deploy_command, index|
+    execute "deploy commands" do
+      docker_with_command = "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} '#{deploy_command}'"
+      Chef::Log.info "Running #{deploy_command} with #{index}"
+      Chef::Log.info docker_with_command
 
+      command docker_with_command
+    end
+  end
   # if (node[:deploy][application][:environment_variables][:db_reload] == "1")
   #   Chef::Log.info "Caylent-Deploy: Attempting to run image with db reload"
   #   execute "copy docker framework" do
@@ -127,11 +139,6 @@ define :docker_deployment_localisation do
   # else
     Chef::Log.info "Caylent-Deploy: Attempting to run image"
     execute "run image" do
-      deploy_commands.each.with_index(1) do |deploy_command, index|
-        Chef::Log.info "Running #{deploy_command} with #{index}"
-        Chef::Log.info "DOCKER COMMAND: docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} '#{deploy_command}'"
-        command "docker run -d #{env_commands} --name #{docker_containerName}_#{index} #{docker_url}/#{docker_application}:#{docker_version} #{deploy_command}"
-      end
       #command "docker run -p 80:80 -p 443:443 #{node[:deploy][application][:environment_variables][:docker_image]}:#{node[:deploy][application][:environment_variables][:docker_version]}"
       command "docker run -d #{env_commands} #{ports} --name #{docker_containerName} #{docker_url}/#{docker_application}:#{docker_version}"
     end
